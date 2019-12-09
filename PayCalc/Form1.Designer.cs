57830f5fadf083af9a53f543b5b7daf75d3dfc57
SinKane <wade.grimm@shaw.ca>
2019-12-09 13:55:48 -0700

    Add project files.

---
+++ PayCalc/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace PayCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblOTPay = new System.Windows.Forms.Label();
+            this.lblRegPay = new System.Windows.Forms.Label();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hours worked";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Rate of pay";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(145, 174);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(127, 37);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "&Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(140, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Overtime pay:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(140, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Regular Pay:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(140, 304);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Total pay:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTotal.ForeColor = System.Drawing.Color.Lime;
+            this.lblTotal.Location = new System.Drawing.Point(311, 304);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(124, 25);
+            this.lblTotal.TabIndex = 8;
+            // 
+            // lblOTPay
+            // 
+            this.lblOTPay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOTPay.ForeColor = System.Drawing.Color.Lime;
+            this.lblOTPay.Location = new System.Drawing.Point(311, 266);
+            this.lblOTPay.Name = "lblOTPay";
+            this.lblOTPay.Size = new System.Drawing.Size(124, 25);
+            this.lblOTPay.TabIndex = 7;
+            this.lblOTPay.Visible = false;
+            // 
+            // lblRegPay
+            // 
+            this.lblRegPay.BackColor = System.Drawing.Color.DimGray;
+            this.lblRegPay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblRegPay.ForeColor = System.Drawing.Color.Lime;
+            this.lblRegPay.Location = new System.Drawing.Point(311, 230);
+            this.lblRegPay.Name = "lblRegPay";
+            this.lblRegPay.Size = new System.Drawing.Size(124, 25);
+            this.lblRegPay.TabIndex = 6;
+            this.lblRegPay.Visible = false;
+            // 
+            // txtHours
+            // 
+            this.txtHours.Location = new System.Drawing.Point(311, 100);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(124, 30);
+            this.txtHours.TabIndex = 9;
+            // 
+            // txtRate
+            // 
+            this.txtRate.Location = new System.Drawing.Point(311, 136);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(124, 30);
+            this.txtRate.TabIndex = 10;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(311, 174);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(124, 37);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(989, 536);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtRate);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblOTPay);
+            this.Controls.Add(this.lblRegPay);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblOTPay;
+        private System.Windows.Forms.Label lblRegPay;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ PayCalc/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PayCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            const decimal FTHOURS = 37.5m;
+            decimal regHours, otHours, regPay, otPay, rate, total;
+            if (Convert.ToDecimal(txtHours.Text) >= FTHOURS)
+            {
+                otHours = Convert.ToDecimal(txtHours.Text) - FTHOURS;
+                regHours = Convert.ToDecimal(txtHours.Text);
+                rate = Convert.ToDecimal(txtRate.Text);
+                lblOTPay.Visible = true;
+                lblRegPay.Visible = true;
+                regPay = regHours * rate;
+                otPay = otHours * (rate * 1.5m);
+                lblRegPay.Text = regPay.ToString("c");
+                lblOTPay.Text = otPay.ToString("c");
+                total = regPay + otPay;
+                lblTotal.Text = total.ToString("c");
+            }
+            else
+            {
+                regHours = Convert.ToDecimal(txtHours.Text);
+                rate = Convert.ToDecimal(txtRate.Text);
+                regPay = regHours * rate;
+                lblTotal.Text = regPay.ToString("c");
+            }
+
+
+
+
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblOTPay.Visible = false;
+            lblRegPay.Visible = false;
+            lblRegPay.Text = "";
+            lblOTPay.Text ="";
+            lblTotal.Text = "";
+            txtHours.Text = "";
+            txtRate.Text = "";
+            txtHours.Focus();
+        }
+    }
+}
